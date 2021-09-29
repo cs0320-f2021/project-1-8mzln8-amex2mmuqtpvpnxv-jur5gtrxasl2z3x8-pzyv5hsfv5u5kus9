c52fba8e1ef3d3b7a00626462981ba1aac74c03a
laura-xu-23 <laura_xu@brown.edu>
2021-09-29 16:47:37 -0400

    knn traversal

--- src/main/java/edu/brown/cs/student/main/KDTree.java
+++ src/main/java/edu/brown/cs/student/main/KDTree.java
@@ -11,7 +11,7 @@ import java.util.PriorityQueue;
 public class KDTree<T extends Comparable<T>> {
   private Node<T> root;
   private final int dimensions;
-  private PriorityQueue<Node<T>> kNearestNeighbors;
+  private PriorityQueue<Node<T>> kNearestNeighbors = new PriorityQueue<>();
 
   /**
    * Constructor for a KDTree object.
@@ -45,16 +45,65 @@ public class KDTree<T extends Comparable<T>> {
       return null;
     }
 
-    int currAxis = depth % this.dimensions;
-    nodeList.sort(new NodeComparator<T>(currAxis));
+    int axis = depth % this.dimensions;
+    nodeList.sort(new NodeComparator<T>(axis));
     int medianIndex = (beginningIndex + endIndex) / 2;
     Node<T> medianNode = nodeList.get(medianIndex);
+    medianNode.setAxis(axis);
     medianNode.setParent(parent);
     medianNode.setLeftChild(createTree(nodeList, depth + 1, beginningIndex,
         medianIndex - 1, medianNode));
     medianNode.setRightChild(createTree(nodeList, depth + 1, medianIndex + 1,
         endIndex, medianNode));
     return medianNode;
+  }
+
+  /**
+   * Helper method to find the k closest neighbors to a set of target coordinates
+   * @param root - the root of the k-d tree we are traversing through
+   * @param k - the number of closest neighbors to find
+   * @param targetCoordinates - the desired set of coordinates to calculate distance to
+   */
+  public void searchForKNearestNeighborsHelper(Node<T> root, int k, List<T> targetCoordinates) {
+    if (root == null) {
+      return;
+    }
+
+    int currAxis = root.getAxis();
+    T rootCoordinate = root.getCoordinates().get(currAxis);
+    T targetCoordinate = targetCoordinates.get(currAxis);
+    root.setDistanceToTarget(targetCoordinates);
+
+    if ((this.kNearestNeighbors.peek() != null
+        && root.getDistanceToTarget() < this.kNearestNeighbors.peek().getDistanceToTarget())
+        || this.kNearestNeighbors.size() < k) {
+      this.kNearestNeighbors.add(root);
+    }
+    if (this.kNearestNeighbors.size() > k) {
+      while (this.kNearestNeighbors.size() > k) {
+        this.kNearestNeighbors.poll();
+      }
+    }
+
+    if (targetCoordinate.compareTo(rootCoordinate) < 0) {
+      searchForKNearestNeighborsHelper(root.getLeftChild(), k, targetCoordinates);
+    } else if (targetCoordinate.compareTo(rootCoordinate) > 0) {
+      searchForKNearestNeighborsHelper(root.getRightChild(), k, targetCoordinates);
+    }
+  }
+
+  /**
+   * Finds the k closest neighbors to a set of target coordinates
+   * @param k - the number of closest neighbors to find
+   * @param targetCoordinates - the desired set of coordinates to calculate distance to
+   * @return a list containing the k nearest neighbors to the given target coordinates
+   */
+  public PriorityQueue<Node<T>> searchForKNearestNeighbors(int k, List<T> targetCoordinates) {
+    if (this.root == null) {
+      return null;
+    }
+    searchForKNearestNeighborsHelper(this.root, k, targetCoordinates);
+    return this.kNearestNeighbors;
   }
 
 }
