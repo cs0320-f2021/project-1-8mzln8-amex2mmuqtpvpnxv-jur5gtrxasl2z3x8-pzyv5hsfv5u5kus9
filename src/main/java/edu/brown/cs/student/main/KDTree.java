3c2d40827d89476856e30dc9dbf2d726ef90149d
laura-xu-23 <laura_xu@brown.edu>
2021-09-28 22:44:56 -0400

    javadoc comments

--- src/main/java/edu/brown/cs/student/main/KDTree.java
+++ src/main/java/edu/brown/cs/student/main/KDTree.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
+/**
+ * Class representing a k-d tree object.
+ * @param <T> - nodes contain data of type T
+ */
 public class KDTree<T extends Comparable<T>> {
   private Node<T> root;
   private final int dimensions;
   private PriorityQueue<Node<T>> kNearestNeighbors;
 
+  /**
+   * Constructor for a KDTree object.
+   * @param dataList - a list containing coordinate data
+   */
   public KDTree(List<List<T>> dataList) {
     List<Node<T>> nodeList = new ArrayList<>();
 
@@ -18,10 +26,21 @@ public class KDTree<T extends Comparable<T>> {
     }
 
     this.dimensions = nodeList.get(0).getCoordinates().size();
-    this.root = createTree(nodeList, 0, 0, nodeList.size() - 1, null);
+    this.root = createTree(nodeList, 0, 0, nodeList.size() - 1,
+        null);
   }
 
-  public Node<T> createTree(List<Node<T>> nodeList, int depth, int beginningIndex, int endIndex, Node<T> parent) {
+  /**
+   * Recursive method to create a k-d tree.
+   * @param nodeList - list of nodes to put into the tree
+   * @param depth - the number of levels from the root of the tree
+   * @param beginningIndex - the starting index of nodes to consider in the list
+   * @param endIndex - the ending index of nodes to consider in the list
+   * @param parent - the parent node
+   * @return the root node of the k-d tree
+   */
+  public Node<T> createTree(List<Node<T>> nodeList, int depth,
+                            int beginningIndex, int endIndex, Node<T> parent) {
     if (nodeList.size() == 0) {
       return null;
     }
--- src/main/java/edu/brown/cs/student/main/Node.java
+++ src/main/java/edu/brown/cs/student/main/Node.java
@@ -2,6 +2,10 @@ package edu.brown.cs.student.main;
 
 import java.util.List;
 
+/**
+ * Class representing a node in a k-d tree.
+ * @param <T> - node coordinates contain data of type T
+ */
 public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
   private final List<T> coordinates;
   private Node<T> leftChild = null;
@@ -9,9 +13,22 @@ public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
   private Node<T> parent = null;
   private double distanceToTarget;
 
+  /**
+   * Constructor for Node object with a list of coordinates.
+   * @param coordinates - list of coordinates to instantiate
+   */
   public Node(List<T> coordinates) {
     this.coordinates = coordinates;
   }
+
+  /**
+   * Constructor for Node object with a list of coordinates, the left and right child nodes,
+   * and the parent node.
+   * @param coordinates - list of coordinates to instantiate
+   * @param leftChild - left child node to instantiate
+   * @param rightChild - right child node to instantiate
+   * @param parent - parent node to instantiate
+   */
   public Node(List<T> coordinates, Node<T> leftChild, Node<T> rightChild, Node<T> parent) {
     this.coordinates = coordinates;
     this.leftChild = leftChild;
@@ -19,28 +36,60 @@ public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
     this.parent = parent;
   }
 
+  /**
+   * Custom compareTo method for Node object that compares the distanceToTarget field.
+   * @param o - Object to compare to
+   * @return the result of the comparison between the distanceToTarget fields of the two Objects
+   */
   @Override
   public int compareTo(Node<T> o) {
     return Double.compare(o.distanceToTarget, this.distanceToTarget);
   }
 
+  /**
+   * Gets the coordinates field of Node object.
+   * @return the coordinates field
+   */
   public List<T> getCoordinates() {
     return coordinates;
   }
 
+  /**
+   * Gets the distanceToTarget field of Node object.
+   * @return the distanceToTarget field
+   */
   public double getDistanceToTarget() {
     return distanceToTarget;
   }
 
+  /**
+   * Sets the left child field with the given Node object.
+   * @param leftChild - Node object to set
+   */
   public void setLeftChild(Node<T> leftChild) {
     this.leftChild = leftChild;
   }
 
+  /**
+   * Sets the right child field with the given Node object.
+   * @param rightChild - Node object to set
+   */
   public void setRightChild(Node<T> rightChild) {
     this.rightChild = rightChild;
   }
 
+  /**
+   * Sets the parent field with the given Node object.
+   * @param parent - Node object to set
+   */
   public void setParent(Node<T> parent) {this.parent = parent; }
+
+
+  /**
+   * Calculates the Euclidean distance between a Node object and the given set of coordinates.
+   * @param targetCoordinates - the desired set of coordinates to calculate distance to
+   * @return the Euclidean distance from the current Node object to the target coordinates
+   */
 
   public double distance(List<T> targetCoordinates) {
     double distanceSquared = 0;
@@ -51,4 +100,5 @@ public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
     this.distanceToTarget = Math.sqrt(distanceSquared);
     return distanceSquared;
   }
+
 }
