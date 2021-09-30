8498d76e2d98ac4c9642c507e435f4fbf937731b
amari-charles <amarichar@gmail.com>
2021-09-29 20:27:36 -0400

    Docs: Added javadocs for User

--- src/main/java/edu/brown/cs/student/main/User.java
+++ src/main/java/edu/brown/cs/student/main/User.java
@@ -10,34 +10,59 @@ public class User {
     private int size;
     private int id;
 
+    /**
+     *
+     * @return - the value of fit
+     */
     public String getFit() {
         return fit;
     }
-
+    /**
+     *
+     * @return - the value of user_id
+     */
     public int getUser_id() {
         return user_id;
     }
-
+    /**
+     *
+     * @return - the value of item_id
+     */
     public int getItem_id() {
         return item_id;
     }
-
+    /**
+     *
+     * @return - the value of rating
+     */
     public int getRating() {
         return rating;
     }
-
+    /**
+     *
+     * @return - the value of rented_for
+     */
     public String getRented_for() {
         return rented_for;
     }
-
+    /**
+     *
+     * @return - the value of category
+     */
     public String getCategory() {
         return category;
     }
-
+    /**
+     *
+     * @return
+     */
     public int getSize() {
         return size;
     }
-
+    /**
+     *
+     * @return
+     */
     public int getId() {
         return id;
     }
