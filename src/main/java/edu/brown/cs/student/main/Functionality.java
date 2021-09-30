b0b86be73c5a11479ae28ca51fbf40230cccbc3d
Mazzaleen <8mzln8@gmail.com>
2021-09-29 21:34:24 -0400

    Worked on moving stars

--- src/main/java/edu/brown/cs/student/main/Functionality.java
+++ src/main/java/edu/brown/cs/student/main/Functionality.java
@@ -6,15 +6,21 @@
 public class Functionality {
  private Hashmap commandHashMap = new Hashmap<String,commandAction>();
 
-commandHashmap.put()
+commandHashmap.put();
 
 
-
-
+    /**
+     * Getter method to return commandHashmap
+     * @return commandHashMap of type hashmap
+     */
     public Hashmap getCommandHashMap() {
         return commandHashMap;
     }
 
+    /**
+     * Getter method that sets hashmap to inputted val
+     * @param commandHashMap -- the hashmap we want to update to
+     */
     public void setCommandHashMap(Hashmap commandHashMap) {
         this.commandHashMap = commandHashMap;
     }
