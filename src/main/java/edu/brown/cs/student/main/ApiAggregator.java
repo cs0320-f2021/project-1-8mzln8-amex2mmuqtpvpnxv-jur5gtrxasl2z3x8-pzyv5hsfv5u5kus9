936001d0cc15118a9ae148cf22ceff6b4aeecf15
amari-charles <amarichar@gmail.com>
2021-10-02 18:02:23 -0400

    Docs: Added Javadocs for ApiAggregator

--- src/main/java/edu/brown/cs/student/main/ApiAggregator.java
+++ src/main/java/edu/brown/cs/student/main/ApiAggregator.java
@@ -11,29 +11,49 @@ import java.util.List;
 public class ApiAggregator {
     private final ApiClient client = new ApiClient();
 
+    /**
+     *
+     * @param dataType - String representing the type of data being considered (Example: rent, review, users)
+     * @return - List of the appropriate objects
+     * @throws Exception
+     */
     public List<Object> getData(String dataType) throws Exception {
         Gson gson = new Gson();
         Type type = setType(dataType);
-        String filepath = "https://runwayapi.herokuapp.com/" + dataType + "-";
-        String response1 = client.makeRequest(ClientRequestGenerator.getSecuredRequest(filepath + "one?auth="));
-        String response2 = client.makeRequest(ClientRequestGenerator.getSecuredRequest(filepath + "three?auth="));
-        response1 = generateExtras("one",filepath,response1);
-        response2 = generateExtras("two",filepath,response2);
+        String filename = "https://runwayapi.herokuapp.com/" + dataType + "-";
+        String response1 = client.makeRequest(ClientRequestGenerator.getSecuredRequest(filename + "one?auth="));
+        String response2 = client.makeRequest(ClientRequestGenerator.getSecuredRequest(filename + "three?auth="));
+        response1 = generateExtras("one",filename,response1);
+        response2 = generateExtras("two",filename,response2);
         String best_response = response1.length() > response2.length() ? response1 : response2;
         return gson.fromJson(best_response,type);
     }
 
-    private String generateExtras(String server, String filepath, String response) {
+    /**
+     * Creates request until the result is not an error message ( At most 5 times )
+     * @param server - The number of the api that we want to call
+     * @param filename - The name of the api we are referencing
+     * @param response - The response generated on the first call
+     * @return - The response as a string
+     */
+    private String generateExtras(String server, String filename, String response) {
         String error_message = "{\"message\": \"Your API call failed due to a malicious error by the course staff\"}";
         try{
         for(int i = 0; i < 5; i++){
             if(response.equals(error_message)){
-                response = client.makeRequest(ClientRequestGenerator.getSecuredRequest(filepath + server + "?auth="));
+                response = client.makeRequest(ClientRequestGenerator.getSecuredRequest(filename + server + "?auth="));
             }else break;
         }
-        }catch (Exception e) {}
+        }catch (Exception ignored) {}
         return response;
     }
+
+    /**
+     *
+     * @param dataType - The type of data we are setting as a String
+     * @return - A Type variable set to the desired type
+     * @throws Exception
+     */
     public Type setType(String dataType) throws Exception {
         Type type;
         if(dataType.equals("rent")){
